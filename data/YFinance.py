45312eb3a29f17cf27a8a2b8d25666a8ee5e3b94
ShiqiangLiu <ShiqiangLiu@savvy.com>
2026-03-04 12:02:44 +0800

    update

--- data/YFinance.py
+++ data/YFinance.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import time
 
 # stock_list = ['AMGN', 'AZO', 'WMT', 'WM', 'GLD', 'TLT']
-stock_list = ['AZO', 'ORLY']
+stock_list = ['TSLA', 'AZO', 'ORLY']
 
 period_ = "25Y"
 
@@ -10,7 +10,7 @@ for ticker in stock_list:
     spy = yf.Ticker(ticker)
     data = spy.history(period=period_, actions=True)
 
-    file_full_path = f"US MKT Raw Data/{ticker}_{period_}_yFinance.csv"
+    file_full_path = f"data/{ticker}_{period_}_yFinance.csv"
     data.to_csv(file_full_path)
 
     print(data.tail())
